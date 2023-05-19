91aa24332c98ab8c6d7d2332cad7f53b80983b8c
Mark <mark@rendlelabs.com>
2023-05-19 15:23:51 +0100

    Use JavaScript to switch themes

--- src/FretWeb/Program.cs
+++ src/FretWeb/Program.cs
@@ -1,6 +1,4 @@
 using FretWeb.Telemetry;
-using FretWeb.Utilities;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -27,44 +25,12 @@ app.UseStaticFiles(new StaticFileOptions
     OnPrepareResponse = ctx => { ctx.Context.Response.Headers[HeaderNames.CacheControl] = cacheHeader; }
 });
 
-app.Use(async (context, next) =>
-{
-    if (context.Request.Query.TryGetValue("theme", out var values))
-    {
-        if (Enum.TryParse(values.FirstOrDefault(), true, out Theme theme))
-        {
-            var themeStr = theme.ToString().ToLower();
-            context.Response.Cookies.Append("fretweb.theme", themeStr);
-            context.Response.Headers.TryAdd("X-Theme", themeStr);
-        }
-
-        var path = context.Request.Path;
-        if (context.Request.Query.Count == 1)
-        {
-            context.Response.Redirect(path);
-        }
-        else
-        {
-            var query = context.Request.QueryString.ToString().Replace($"&theme={theme.ToString().ToLower()}", "");
-            context.Response.Redirect($"{path}{query}");
-        }
-
-        return;
-    }
-    await next();
-});
-
 app.UseRouting();
 
 app.Use(async (context, next) =>
 {
-    // context.Response.Headers.CacheControl = cacheHeader;
-    // context.Response.Headers.Vary = "X-Theme, Accept-Encoding";
-    if (!context.Request.Cookies.TryGetValue("fretweb.theme", out var theme))
-    {
-        theme = "light";
-    }
-    context.Response.Headers.TryAdd("X-Theme", theme);
+    context.Response.Headers.CacheControl = cacheHeader;
+    context.Response.Headers.Vary = "Accept-Encoding";
     await next();
 });
 
--- src/FretWeb/wwwroot/js/site.js
+++ src/FretWeb/wwwroot/js/site.js
@@ -2,3 +2,46 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+window.addEventListener('DOMContentLoaded', e => {
+  const theme = localStorage.getItem('theme');
+  const lightLink = document.querySelector('#theme-light-link');
+  const darkLink = document.querySelector('#theme-dark-link');
+  const themeStylesheet = document.querySelector('#theme-stylesheet');
+  const fretboardStylesheet = document.querySelector('#fretboard-stylesheet');
+  const mainNav = document.querySelector('#main-nav');
+  
+  function setLight() {
+    themeStylesheet.href = '/lib/bootswatch/flatly.min.css';
+    fretboardStylesheet.href = '/css/fretboard-light.css';
+    mainNav.classList.remove('navbar-dark');
+    mainNav.classList.add('navbar-light');
+    localStorage.setItem('theme', 'light');
+    darkLink.classList.remove('d-none');
+    lightLink.classList.add('d-none');
+  }
+  
+  function setDark() {
+    themeStylesheet.href = '/lib/bootswatch/darkly.min.css';
+    fretboardStylesheet.href = '/css/fretboard-dark.css';
+    mainNav.classList.remove('navbar-light');
+    mainNav.classList.add('navbar-dark');
+    lightLink.classList.remove('d-none');
+    darkLink.classList.add('d-none');
+    localStorage.setItem('theme', 'dark');
+  }
+  
+  if (theme === 'dark') {
+    setDark();
+  } else {
+    setLight();
+  }
+  lightLink.addEventListener('click', e => {
+    e.preventDefault();
+    setLight();
+  });
+  darkLink.addEventListener('click', e => {
+    e.preventDefault();
+    setDark();
+  });
+});
